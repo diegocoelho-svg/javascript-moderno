9ccf0308618a381c5adfc6f38534a965b759887d
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-30 08:54:29 -0300

    Conhecendo rest params

--- scripts.js
+++ scripts.js
@@ -1,19 +1,12 @@
-// destructuring assignment (desestruturação) permite extrair dados de arrays ou objetos em variáveis distintas.
+// Rest params (...) permite representar um número indefinido de argumentos como um array.
 
-const product = {
-  description: "Teclado",
-  price: 150
+function values (a, ...rest) {
+  // Mostra a quantidade de parametros.
+  console.log(rest.length)
+  // Exibindo o conteúdo do array.
+  console.log(...rest)
+  // Exibe o conteúdo do rest que é um array.
+  console.log(rest)
 }
 
-const { description, price } = product
-
-function newProduct({description, price}) {
-console.log("### NOVO PRODUTO ###")
-console.log("Descrição:", description)
-console.log("Preço: R$", price)
-}
-
-newProduct({
-  description: "Mouse",
-  price: 70,
-})
+values(2, 1, 3, 4)
