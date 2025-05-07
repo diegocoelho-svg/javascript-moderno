f021ebd682f9deda6f5997ded6d665011c318c64
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-07 19:15:30 -0300

    conhecendo promises

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-// setTimeout() Será executada após um intervalo de tempo especificado.
+// Função que retorna uma promise.
+function asyncFunction() {
+  return new Promise((resolve, reject) => {
+    // Simula uma operação assíncrona
+    setTimeout(() => {
+      const isSuccess = true
 
-setTimeout(() => {
-  console.log("Olá, tudo bem?")
-}, 5000); //tempo em milissegundos
+      if(isSuccess) {
+        resolve("A operação foi concluída com sucesso!")
+      } else {
+        reject("Algo deu errado")
+      }
+    }, 3000) // Simula uma operação que leva 3 segundos
+  })
+}
+// Visualizando que o retorno é uma promise.
+// console.log(asyncFunction())
 
+console.log("Executando função assíncrona...")
 
-
-// setInterval() executa uma função após um intervalo de tempo especificado. Diferente do timeout, ele executa a cada (tempo estimado)
-
-let value = 10
-
-const interval = setInterval(() => {
-  console.log(value)
-  value--
-  if(value === 0) {
-    console.log("Feliz Ano Novo")
-
-    // Interrompe o intervalo de execuções.
-    clearInterval(interval)
-  }
-}, 1000);
-
+// Quando a Promise for resolvida, execute ESTA função (Função dentro de função = Callback Function) com o resultado.
+asyncFunction().then((response) => {
+  console.log("Sucesso:", response)
+}).catch((error) => {
+  console.log("Error:", error)
+}).finally(() => {
+  console.log("Fim da execução")
+}) 
