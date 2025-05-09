80624afaa4da8f033705df96888944e683563c95
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-08 22:15:05 -0300

    package (day.js)

--- main.js
+++ main.js
@@ -1,21 +1,2 @@
-// (1) Executa o código de forma síncrona e o valor 1 é impresso imediatamente no console.
-
-console.log(1)
-
-// (3) Microtasks são executadas antes de temporizadores e promises.
-queueMicrotask(() => {
-  console.log(2)
-})
-
-// (5) Macrotasks que aguarda o evento de temporizador ser acionado.
-setTimeout(() => {
-  console.log(3)
-}, 1000);
-
-// (2) Execução síncrona. (não necessita de async await)
-console.log(4)
-
-// (4) Adiciona uma microtask
-Promise.resolve(true).then(() => {
-  console.log(5)
-})
+const now = dayjs()
+console.log(now.format("DD/MM - HH:mm")) // Olhar a documentação
